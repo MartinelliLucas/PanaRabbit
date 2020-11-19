95aa14f65a82b0a2d48ad7b60d9d98cf9a8d09ac
LAPTOP-C4R5709C\info <nahuel.monin@gmail.com>
2020-11-19 16:51:45 -0300

    Agregue el metodo para respawnear los autos destruidos. Aca aun no esta implementada la pantalla de game over cuando el conejo muere.

--- src/juego/Juego.java
+++ src/juego/Juego.java
@@ -18,6 +18,7 @@ public class Juego extends InterfaceJuego
 	private Kamehameha kame;
 	private Kamehameha circulo;
 	private boolean flagCd = false;
+		
 	// Variables y métodos propios de cada grupo
 	
 	// metodo enfriamiento kame y auto ;
@@ -39,6 +40,26 @@ public class Juego extends InterfaceJuego
 		timer.schedule(tarea, 5000);
 	}
 		
+	//metodo para respawnear autos:
+	void carRespawn(Auto[] arrAuto) {
+		Timer carTimer = new Timer();
+		
+		TimerTask respawn = new TimerTask() {
+			@Override
+			public void run() {
+				for (int i = 0; i < arrAuto.length; i++) {
+					if( i == 0 && arrAuto[i] == null) {
+						arrAuto[i] = new Auto(arrAuto[i+1].getX()-250,arrAuto[i+1].getY(),60,40);
+					}
+					if (arrAuto[i] == null) {
+						arrAuto[i] = new Auto(arrAuto[i-1].getX()+250,arrAuto[i-1].getY(),60,40);
+					}
+				}
+			}
+		};
+		carTimer.schedule(respawn, 5000);
+	}
+	
 	
 	//metodo para identificar auto colisionado con el kame:
 	
@@ -50,14 +71,12 @@ public class Juego extends InterfaceJuego
 			if (arrAuto[i] == null)
 				continue;
 			if (arrAuto[i].getX() - (arrAuto[i].getWidth()/2) < kame.getX() &&
-					kame.getX() < (arrAuto[i].getX() + (arrAuto[i].getWidth()/2)) &&
-					arrAuto[i].getY() + (arrAuto[i].getHeight()/2) > kame.getY() - (kame.getAlto()/2) &&
-					kame.getY() > (arrAuto[i].getY() - arrAuto[i].getHeight()/2)) {
+				kame.getX() < (arrAuto[i].getX() + (arrAuto[i].getWidth()/2)) &&
+				arrAuto[i].getY() + (arrAuto[i].getHeight()/2) > kame.getY() - (kame.getAlto()/2) &&
+				kame.getY() > (arrAuto[i].getY() - arrAuto[i].getHeight()/2)) {
 				return i;
 			}
 		}
-		
-		
 		return -1;
 	}
 	
@@ -66,10 +85,10 @@ public class Juego extends InterfaceJuego
 			if (arrAuto[i] == null) {
 				continue;
 			}
-			if (conejo.getY() > arrAuto[i].getY() - arrAuto[i].getHeight()/2 && 
-				conejo.getY() < arrAuto[i].getY() + arrAuto[i].getHeight()/2 &&
-				conejo.getX() > arrAuto[i].getX() - arrAuto[i].getWidth()/2 &&
-				conejo.getX() < arrAuto[i].getX() + arrAuto[i].getWidth()/2){
+			if (conejo.getY() + conejo.getHeight()/2 > arrAuto[i].getY() - arrAuto[i].getHeight()/2 && 
+				conejo.getY() - conejo.getHeight()/2 < arrAuto[i].getY() + arrAuto[i].getHeight()/2 &&
+				conejo.getX() + conejo.getWidth()/2> arrAuto[i].getX() - arrAuto[i].getWidth()/2 &&
+				conejo.getX() - conejo.getWidth()/2< arrAuto[i].getX() + arrAuto[i].getWidth()/2){
 				return true;
 			}
 		}
@@ -157,9 +176,13 @@ public class Juego extends InterfaceJuego
 			if (colisionAuto(this.autosCalle, this.kame) != -1){
 				this.autosCalle[colisionAuto(this.autosCalle,this.kame)] = null;
 				this.kame = null;
-			}			
-		}
+				carRespawn(autosCalle);
+			}
 			
+		}
+		
+		
+		
 		for (int i = 0; i < this.autosCalle2.length; i++) {
 			if (autosCalle2[i] == null) {
 				continue;
@@ -180,10 +203,10 @@ public class Juego extends InterfaceJuego
 			if (colisionAuto(this.autosCalle2, this.kame) != -1){
 				this.autosCalle2[colisionAuto(this.autosCalle2,this.kame)] = null;
 				this.kame = null;
+				carRespawn(autosCalle2);
 			}			
 		}
-		
-		
+				
 		//Creacion, movimiento e interacciones del Kamehameha:
 
 		//crea el objeto kame (que se inicializa como null), asiga true al flag para dibujar y false al flag del cd.
